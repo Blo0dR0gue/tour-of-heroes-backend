13d3c502a8168d285b26b0377ee4d1083722d061
Daniel Czeschner <daniel.czeschner@web.de>
2022-09-28 11:51:14 +0200

    Added mapping to get a single hero

--- src/main/java/com/example/demo/hero/HeroService.java
+++ src/main/java/com/example/demo/hero/HeroService.java
@@ -2,6 +2,8 @@ package com.example.demo.hero;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,11 @@ public class HeroService {
 
     public List<Hero> getAll(){
         return repository.findAll();
+    }
+
+    public Hero getById(int heroId) {
+        Hero hero  = this.repository.findById(heroId).orElseThrow(() -> new EntityNotFoundException("Hero with id " + String.valueOf(heroId) + " not found"));
+        return hero;
     }
 
 }
